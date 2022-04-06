94b4483265265de328905e37c82e3861e8042d7d
mochammad faisal <bullenxz12@gmail.com>
2022-04-06 19:28:07 +0700

    add user, channel, subscriber routes

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const { append } = require("express/lib/response");
 const router = express.Router();
 
 const channel = require("./channels");
 const user = require("./users");
+const subscriber = require("./subscribers");
 
 router.get("/", (req, res) => {
   res.status(200).json({
     status: "success",
-    message: "welcome to portal berita api",
+    message: "welcome to youtube api",
     data: null,
   });
 });
+
+router.use("/users", user);
+router.use("/channels", channel);
+router.use("/subscribers", subscriber);
 
 module.exports = router;
